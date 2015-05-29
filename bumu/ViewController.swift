0c0a85184adfd8b9090d6da79592409bb599068f
Sarp Oğulcan Solakoğlu <sosolakoglu@Sarps-iMac.local>
2015-05-29 23:16:44 +0300

    Initial Commit

---
+++ bumu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bumu
+//
+//  Created by Sarp Oğulcan Solakoğlu on 29/05/15.
+//  Copyright (c) 2015 Sarp Oğulcan Solakoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
